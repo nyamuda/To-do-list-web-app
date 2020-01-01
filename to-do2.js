a935c62a2d0019851b01abcf807ab8c001ff2ac9
Peirce Nyamuda <ptnrlab@gmail.com>
2020-01-01 16:59:57 +0200

    first draft of sign up or sign in page completed

--- to-do2.js
+++ to-do2.js
@@ -8,30 +8,58 @@ var passwordIn = document.querySelector("#password-in");
 var passwordUp = document.querySelector("#password-up");
 var formUp = document.querySelector(".form-up");
 var formIn = document.querySelector(".form-in");
+var upCancel=document.querySelector("#up-cancel");
+var nameTaken=document.querySelector("#user-taken");
+var passwordRules=document.querySelector(".password-errors");
+
 var name = "";
 var password = "";
 var userData = {};
 var specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 var numbers = "0123456789";
-/*
+
+
+
+//on clicking the sign up button,the sign up form will pop up
 signUp1.onclick=function() {
-    formUp.style.display="block";
+    formUp.style.transform="scale(1)";
+	formUp.style.transition="transform 0.5s ease-in-out";
+//the sign up and sign in forms must not pop up at the same time on clicking both both buttons;
+	formIn.style.transform="scale(0)";
+	formIn.style.transition="transform 0.01s ease-in-out";
+	
 }
 
+upCancel.onclick=function() {
+	formUp.style.transform="scale(0)";
+	formUp.style.transition="transform 0.01s ease-in-out";
+	passwordRules.style.display="none";
+	}
+
+
 signIn1.onclick=function() {
-    formIn.style.display="block"
-}*/
+    formIn.style.transform="scale(1)";
+	formUp.style.transform="scale(0)";
+	formUp.style.transition="transform 0.5s ease-in-out";
+	formIn.style.transition="transform 0.5s ease-in-out";
+	passwordRules.style.display="none";
+	
+}
+
 inputUp.oninput = function() {
 	name = inputUp.value;
 };
 passwordUp.oninput = function() {
 	password = passwordUp.value;
 };
+
+//there must be no spaces in the username input when signing up
 for (var i = 0; i < name.length; i++) {
 	if (name[i] == " ") {
 		alert("no spaces");
 	}
 }
+//there must be no spaces in the password input when signing up
 for (var i = 0; i < password.length; i++) {
 	if (name[i] == " ") {
 		alert("no spaces");
@@ -69,20 +97,32 @@ signUpForm.onclick = function(event) {
 			smallLetter.length >= 1 &&
 			numericalValue.length >= 1
 		) {
+			//getting the data from the local storage
 			var getData = JSON.parse(localStorage.getItem("theData"));
+			//if there is nothing in the local storage
 			if (getData == null) {
 				userData[name] = password;
 				localStorage.setItem("theData", JSON.stringify(userData));
-			} else {
+			} 
+			//checking if the new username doesn't already exist in the local storage
+			else {
 				if (name.valueOf() in getData) {
-					alert("username exists");
-				} else {
+					nameTaken.style.display="block";
+					event.preventDefault();
+				}
+		
+				else {
 					userData[name] = password;
 					localStorage.setItem("theData", JSON.stringify(userData));
+					event.preventDefault()
+					window.location.href="todo.html";
 				}
 			}
-		} else {
-			alert("passoword error");
+		} 
+		// if one of the password requirements is not met
+		else {
+			passwordRules.style.display="block";
+			event.preventDefault();
 		}
 	}
 };
